a5d2c14ac28a6a89e3c8d1afb3c21c5df2f54882
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-05-02 16:17:48 +0300

    Задание№2
    Видео плеер

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time"
+
+const onPlay = function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay,1000));
+
+let savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+    player.setCurrentTime(savedData).then(function (seconds) {}).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+}
+
+
+
 
