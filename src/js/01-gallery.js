6b480bebf1bb7cea49b5b691441e647e197f34a0
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-05-02 15:14:36 +0300

    Задание №1
    Библиотека SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(galleryItems) {
+ return galleryItems.map(item => {
+    return ` <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+  ` 
+    }).join("");
+ 
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.innerHTML = galleryMarkup;
+
+
+galleryList.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+   event.preventDefault();
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+
